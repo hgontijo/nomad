a4769a3464e95c8d620b9ac5d72e5350bbeb3b31
Sean Chittenden <sean@chittenden.org>
2016-05-21 20:15:58 -0700

    Create a `nomad/structs/config` to break an import cycle.
    Flattening and normalizing the various Consul config structures and
    services has led to an import cycle.  Break this by creating a new package
    that is intended to be terminal in the import DAG.

--- command/agent/config.go
+++ command/agent/config.go
@@ -86,11 +86,7 @@ type Config struct {
 	// Consul contains the configuration for the Consul Agent and
 	// parameters necessary to register services, their checks, and
 	// discover the current Nomad servers.
-<<<<<<< 4fc5c25557779328f380047adcb643df78599035
 	Consul *config.ConsulConfig `mapstructure:"consul"`
-=======
-	Consul *ConsulConfig `mapstructure:"consul"`
->>>>>>> Distill config.Config.ConsulConfig down to config.Config.Consul
 
 	// NomadConfig is used to override the default config.
 	// This is largly used for testing purposes.
@@ -134,65 +130,6 @@ type AtlasConfig struct {
 	Endpoint string `mapstructure:"endpoint"`
 }
 
-<<<<<<< 4fc5c25557779328f380047adcb643df78599035
-=======
-// ConsulConfig contains the configuration information necessary to
-// communicate with a Consul Agent in order to:
-//
-// - Register services and checks with Consul
-//
-// - Bootstrap this Nomad Client with the list of Nomad Servers registered
-//   with Consul
-type ConsulConfig struct {
-
-	// ServerServiceName is the name of the service that Nomad uses to register
-	// servers with Consul
-	ServerServiceName string `mapstructure:"server_service_name"`
-
-	// ClientServiceName is the name of the service that Nomad uses to register
-	// clients with Consul
-	ClientServiceName string `mapstructure:"client_service_name"`
-
-	// AutoRegister determines if Nomad will register the Nomad client and
-	// server agents with Consul
-	AutoRegister bool `mapstructure:"auto_register"`
-
-	// Addr is the address of the local Consul agent
-	Addr string `mapstructure:"addr"`
-
-	// Token is used to provide a per-request ACL token.This options overrides
-	// the agent's default token
-	Token string `mapstructure:"token"`
-
-	// Auth is the information to use for http access to Consul agent
-	Auth string `mapstructure:"auth"`
-
-	// EnableSSL sets the transport scheme to talk to the Consul agent as https
-	EnableSSL bool `mapstructure:"ssl"`
-
-	// VerifySSL enables or disables SSL verification when the transport scheme
-	// for the consul api client is https
-	VerifySSL bool `mapstructure:"verify_ssl"`
-
-	// CAFile is the path to the ca certificate used for Consul communication
-	CAFile string `mapstructure:"ca_file"`
-
-	// CertFile is the path to the certificate for Consul communication
-	CertFile string `mapstructure:"cert_file"`
-
-	// KeyFile is the path to the private key for Consul communication
-	KeyFile string `mapstructure:"key_file"`
-
-	// ServerAutoJoin enables Nomad servers to find peers by querying Consul and
-	// joining them
-	ServerAutoJoin bool `mapstructure:"server_auto_join"`
-
-	// ClientAutoJoin enables Nomad servers to find addresses of Nomad servers
-	// and register with them
-	ClientAutoJoin bool `mapstructure:"client_auto_join"`
-}
-
->>>>>>> Distill config.Config.ConsulConfig down to config.Config.Consul
 // ClientConfig is configuration specific to the client mode
 type ClientConfig struct {
 	// Enabled controls if we are a client
@@ -438,11 +375,7 @@ func DefaultConfig() *Config {
 		Addresses:      &Addresses{},
 		AdvertiseAddrs: &AdvertiseAddrs{},
 		Atlas:          &AtlasConfig{},
-<<<<<<< 4fc5c25557779328f380047adcb643df78599035
 		Consul: &config.ConsulConfig{
-=======
-		Consul: &ConsulConfig{
->>>>>>> Distill config.Config.ConsulConfig down to config.Config.Consul
 			ServerServiceName: "nomad-server",
 			ClientServiceName: "nomad-client",
 			AutoRegister:      true,
